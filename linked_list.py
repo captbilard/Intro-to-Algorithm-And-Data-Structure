a36d6350cf91df33845ba233a69317c3b48af1e8
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-12-12 00:30:40 +0100

    Created an insert() method

--- linked_list.py
+++ linked_list.py
@@ -55,7 +55,30 @@ class Linked_List:
                 current = current.next_node  
         return None
 
-    
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at the index position
+        Insertion takes O(1) (constant time) but finding the node at the
+        insertion points takes O(n) time.
+        Takes overall O(n) time
+        """
+        if index == 0:
+            return self.add(data)
+        
+        new_node = Node(data)
+        current  = self.head
+
+        position = index
+
+        while position > 1:
+            current = current.next_node
+            position -= 1
+        
+        prev_node = current.next_node
+        new_node.next_node = prev_node
+        current.next_node = new_node 
+
+
     def __repr__(self):
         nodes = []
         current = self.head
